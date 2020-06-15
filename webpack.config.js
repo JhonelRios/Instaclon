2973f3812d2627d10226cd6b4061a7ee83ce784c
Jhonel Rios Jaimes <riosjaimes.j@gmail.com>
2020-06-15 10:49:40 -0500

    webpack optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   devtool: 'inline-source-map',
@@ -23,9 +24,6 @@ module.exports = {
       }
     ]
   },
-  stats: {
-    warningsFilter: [/Failed to parse source map/]
-  },
   devServer: {
     historyApiFallback: true,
     proxy: [
@@ -34,6 +32,48 @@ module.exports = {
         target: 'http://localhost:5001/instaclon-96907/us-central1'
       }
     ]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new UglifyJSPlugin({
+        uglifyOptions: {
+          sourceMap: true,
+          compress: {
+            drop_console: true,
+            conditionals: true,
+            unused: true,
+            comparisons: true,
+            dead_code: true,
+            if_return: true,
+            join_vars: true
+          },
+          output: {
+            comments: false
+          }
+        }
+      })
+    ],
+    splitChunks: {
+      chunks: 'async',
+      name: true,
+      cacheGroups: {
+        vendors: {
+          name: 'vendors',
+          chunks: 'all',
+          reuseExistingChunk: true,
+          priority: 1,
+          filename: 'vendor.js',
+          enforce: true,
+          test(module, chunks) {
+            const name = module.nameForCondition && module.nameForCondition();
+            return chunks.some(
+              (chunk) => chunk.name !== 'vendors' && /[\\/]node_modules[\\/]/.test(name)
+            );
+          }
+        }
+      }
+    }
   },
   plugins: [
     new HtmlWebpackPlugin({
